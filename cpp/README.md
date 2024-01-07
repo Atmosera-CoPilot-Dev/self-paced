54a17b727e75abf1a8da20587d7d5820fe3eef60
Andrew Scoppa <andrew-scoppa@github.com>
2024-01-07 15:27:46 -0500

    improved cpp

---
+++ cpp/README.md
@@ -0,0 +1,17 @@
+# C++ Vector
+
+This exercise will give you an opportunity to use prompt engineering to implement the methods of a Vector class in the `starter` folder.
+
+## Instructions
+
+- Use CoPilot to find the file containing the entrypoint `main()`. This file does not need to be changed.
+
+- Use Copilot to implement the vector methods in the file Vector.cpp
+
+- There are 5 methods that you need to implement. The first two are the constructor and destructor. The constructor initializes the m_vec member variable as a new integer array of size size. The destructor deallocates the memory that was previously allocated for the m_vec array in the constructor.
+
+- The next two methods are Set and Print. The Set method sets the values of the m_vec array. It takes a pointer to an integer array as an argument and copies the values from the input array to m_vec. It assumes that the input array has at least size elements. The Print method prints the elements of the m_vec array to the console using cout. Each element is followed by two spaces.
+
+- The last method is an overloaded multiplication operator. It takes two Vector objects as arguments and calculates the dot product of their m_vec arrays. The dot product is calculated as the sum of the products of the corresponding elements in the two arrays.
+
+- Once you have implemented all the methods, compile and run the program.
--- cpp/solution/Vector.cpp
+++ cpp/solution/Vector.cpp
@@ -1,25 +1,35 @@
-// Vector.cpp
-
+// Implement Vector class methods
 #include <iostream>
 #include "Vector.h"
-using namespace std;
+using namespace std; // remove if desired
 
+ // Implement the constructor for the Vector class. 
+ // It initializes the m_vec member variable as a new integer array of size Vector::size.
 Vector::Vector()
 {
 	m_vec = new int[Vector::size];
 }
 
+
+ // Implement the destructor for the Vector class.
+ // It deallocates the memory that was previously allocated for the m_vec array in the constructor.
 Vector::~Vector()
 {
 	delete [] m_vec;
 }
 
+ // Implement the Vector::Set(int *arr) method for the Vector class.
+ // It takes a pointer to an integer array as an argument and copies the values from the input array to m_vec.
+ // It assumes that the input array has at least Vector::size elements.
 void Vector::Set(int *arr)
 {
 	for (int i = 0; i < Vector::size; i++)
 		m_vec[i] = arr[i];
 }
-		
+
+ // Implement the Vector::Print() method.
+ // It prints the elements of the m_vec array to the console using std::cout.
+ // Each element is followed by two spaces.
 void Vector::Print()
 {
 	for (int i = 0; i < Vector::size; i++)
@@ -27,6 +37,10 @@ void Vector::Print()
 	cout << endl;
 }
 
+ // Implement an overloaded multiplication operator. 
+ // It takes two Vector objects as arguments and calculates the dot product of their m_vec arrays. 
+ // The dot product is calculated as the sum of the products of the corresponding elements in the two arrays.
+ // For example, if the two arrays are [1, 2, 3] and [1, 4, 9], the dot product is 1*1 + 2*4 + 3*9 = 1 + 8 + 27 = 36.
 int operator *(const Vector& v1,
 			   const Vector& v2)
 {
