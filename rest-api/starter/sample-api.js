42613071e9844a17c85e786b787818b17f0e4342
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-01-19 07:42:32 -0500

    Update sample-api.js

--- rest-api/starter/sample-api.js
+++ rest-api/starter/sample-api.js
@@ -1,8 +1,5 @@
 // docs:  https://docs.github.com/en/rest/reference/issues#list-repository-issues
 
-// import the Octokit library
-import { Octokit } from "octokit"
-// import prompt-sync library
-import promptSync from 'prompt-sync';
+
 
 
